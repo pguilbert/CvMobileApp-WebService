7d1f87807006c14364e5369b699aff6158b87bb7
Paul Guilbert <guilbert.paul@gmail.com>
2013-12-31 12:00:32 +0100

    Change name of solution

---
+++ ClassMappers/MissionMapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Interfaces;
+using MissionDAO = DataAccess.Model.Mission;
+using MissionDTO = Common.Mission;
+
+namespace ClassMappers
+{
+    public class MissionMapper : IMapper<MissionDAO, MissionDTO>
+    {
+        public MissionDAO ToDAO(MissionDTO element)
+        {
+            return new MissionDAO
+            {
+                Id = element.Id,
+                DateDebut = element.DateDebut,
+                DateFin = element.DateFin,
+                Description = element.Description,
+                LibelleMission = element.LibelleMission,
+                Poste = element.Poste
+            };
+        }
+
+        public MissionDTO ToDTO(MissionDAO element)
+        {
+            return new MissionDTO
+            {
+                Id = element.Id,
+                DateDebut = element.DateDebut,
+                DateFin = element.DateFin,
+                Description = element.Description,
+                LibelleMission = element.LibelleMission,
+                Poste = element.Poste
+            };
+        }
+    }
+}
